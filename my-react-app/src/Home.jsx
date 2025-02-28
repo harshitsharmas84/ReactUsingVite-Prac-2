6c5ce00142108fd9020b32e46ceeb37ba0ad3a0f
harshitsharmas84 <harshit.sharma.s84@kalvium.community>
2025-02-28 10:04:29 +0530

    Done

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Harshit Sharma</h1>
+      <p>Contact: Harshit@example.com</p>
+    </div>
+  );
+};
+export default Home;
